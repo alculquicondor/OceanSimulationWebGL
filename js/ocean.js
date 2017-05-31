172baded983a68fe9d0b26a9b27f6e9c8f0892b3
Aldo Culquicondor <alculquicondor@gmail.com>
2017-05-31 15:50:42 -0400

    closer light

--- js/ocean.js
+++ js/ocean.js
@@ -152,7 +152,7 @@ class Ocean {
             gl.uniform4fv(this.waves, this.wavesData);
         }
         if (this.lightWorld !== undefined) {
-            gl.uniform3f(this.lightWorld, 60.0, 500.0, -20.0);
+            gl.uniform3f(this.lightWorld, 60.0, 300.0, -20.0);
         }
         if (this.texture !== undefined && this.normalTex !== undefined) {
             gl.activeTexture(gl.TEXTURE0);
